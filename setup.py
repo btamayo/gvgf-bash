1fb91528604b620f307da38cf34b1842fa44cf2e
Bianca Tamayo <bianca@biancatamayo.me>
2017-06-03 21:18:26 -0700

    Initial Package Setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='gvgf',
+      version='0.1',
+      description='gvgf',
+      url='http://github.com/btamayo/gvgf',
+      author='Bianca Tamayo',
+      author_email='hi@biancatamayo.me',
+      license='MIT',
+      packages=['gvgf'],
+      zip_safe=False,
+      dependency_links=['https://github.com/petervanderdoes/gitflow-avh'])
