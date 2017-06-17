f4025250cf14f90c82faaca64d2045cd6a0f9cc8
Bianca Tamayo <bianca@biancatamayo.me>
2017-06-16 22:48:04 -0700

    Forgot to commit file
    FullSemver: 0.1.0+50
    Branch: master

---
+++ setup_docker_testenv.sh
@@ -0,0 +1,9 @@
+# Run before any tests if GitVersion is not installed to symlink it to ~/.bash_profile
+
+if [ "$DOCKER_ENV" = "1" ]
+then
+  # PWD is project root
+  echo $USER
+  echo "alias gitversion='mono \"$(pwd)\"/GitVersion_3.6.5/GitVersion.exe'\" >> /root/.bashrc"
+  cd gvgf/tests && ./init_run.sh
+fi
